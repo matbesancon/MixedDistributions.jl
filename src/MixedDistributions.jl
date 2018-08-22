5422ca1138f5b4a99576b5a3d2f7c26636aae674
mbesancon <mathieu.besancon@gmail.com>
2018-08-22 18:11:37 +0200

    statistics as dep, fixed tests

--- src/MixedDistributions.jl
+++ src/MixedDistributions.jl
@@ -6,20 +6,36 @@ import Statistics: mean
 import Distributions
 const Dst = Distributions
 
-export MixedDistribution
+export MixedDistribution, MassPoint
+
+"""
+    A mass point in a discrete or mixed distribution is represented by its probability p and position x
+"""
+struct MassPoint
+    p::Float64
+    x::Float64
+end
+# TODO replace p and x vecs with MassPoint
 
 """
     A mixed continuous-discrete distribution
 """
-struct MixedDistribution <: Dst.AbstractMixtureModel{Dst.Univariate, Dst.Continuous}
+struct MixedDistribution{DT<:Dst.ContinuousUnivariateDistribution} <: Dst.ContinuousUnivariateDistribution
     mass_probs::Vector{Float64}
     mass_points::Vector{Float64}
     cont_weights::Vector{Float64}
-    cont_dists::Vector{Dst.ContinuousUnivariateDistribution}
-    function MixedDistribution(mass_probs, mass_points, cont_weights, cont_dists)
-        length(mass_probs) == length(mass_points)  || ArgumentError("Mass length inconsistency") 
+    cont_dists::Vector{DT}
+    function MixedDistribution(mass_probs::Vector{Float64}, mass_points::Vector{Float64}, cont_weights::Vector{Float64}, cont_dists::Vector{DT}) where {DT <: Dst.ContinuousUnivariateDistribution}
+        length(mass_probs) == length(mass_points)  || ArgumentError("Mass length inconsistency")
         length(cont_weights) == length(cont_dists) || ArgumentError("Continuous length inconsistency")
-        new(mass_probs, mass_points, cont_weights, cont_dists)
+        # sort mass points
+        pairs = sort(zip(mass_points, mass_probs) |> collect, by = t -> t[1])
+        mass_points = [t[1] for t in pairs]
+        mass_probs =  [t[2] for t in pairs]
+        sum(mass_probs) + sum(cont_weights) ≈ one(eltype(mass_probs)) && return new{DT}(mass_probs, mass_points, cont_weights, cont_dists)
+        @warn "Prior probabilities not normalized"
+        s = sum(mass_probs) + sum(cont_weights)
+        new{DT}(mass_probs./s, mass_points, cont_weights./s, cont_dists)
     end
 end
 
@@ -54,11 +70,13 @@ function Dst.pdf(md::MixedDistribution, x::Real)
 end
 
 function Dst.cdf(md::MixedDistribution, x::Real)
-    mass_part = sum(pᵢ for (xᵢ, pᵢ) in zip(md.mass_points, md.mass_probs) if xᵢ <= x)
+    mass_part = sum(pᵢ for (pᵢ, xᵢ) in zip(md.mass_probs, md.mass_points) if xᵢ <= x)
     cont_part = sum(μᵢ * Dst.cdf(dᵢ, x) for (dᵢ, μᵢ) in (md.cont_dists, md.cont_weights))
     return mass_part + cont_part
 end
 
 Dst.ncomponents(md::MixedDistribution) = Dst.ncategories(md) + length(md.cont_dists)
+
+Dst.probs(md::MixedDistribution) = vcat(md.mass_probs, md.cont_weights)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,12 @@ const u1 = Dst.Uniform()
 const u2 = Dst.Uniform(2.0,2.5)
 
 @testset "Base interface" begin
-    m1 = MixedDistribution([],[],[0.4,0.6], [u1, u2])
-    @test maximum(m1) ≈ maximum(u2)
-    @test minimum(m1) ≈ minimum(u1)     
+    m = MixedDistribution(Float64[],Float64[],[0.4,0.6], [u1, u2])
+    @test maximum(m) ≈ maximum(u2)
+    @test minimum(m) ≈ minimum(u1)
+end
+
+@testset "Mixed properties" begin
+    m = MixedDistribution([0.1,0.05,0.05],[1.,7.,3.],[0.2,0.6], [u1, u2])
+    @test issorted(m.mass_points)
 end
